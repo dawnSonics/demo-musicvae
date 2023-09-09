79c09b7fd499a5f6654cfcaeaa4790f43aa2a6b0
aksaw <akshaysawhney1@gmail.com>
2023-09-09 07:02:01 -0700

    add temperature slider

--- demo.js
+++ demo.js
@@ -1,13 +1,14 @@
 canvas = document.getElementById('canvas')
 button = document.getElementById('gen-button')
 select = document.getElementById('gen-select')
+slider = document.getElementById('gen-slider')
 
 function changeButtonText(val) {
     button.value = val
     button.innerHTML = val
 }
 
-const vae_temperature = 0.5
+var vae_temperature = 0.5
 const controlArgs = {
     chordProgression: ["Dm", "F", "Am", "G"]
 }
@@ -24,10 +25,14 @@ const vizConfig = {
 // music_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_chords');
 // music_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_16bar_small_q2');
 
+melody2bar_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small');
 melody_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_lokl_q2');
 drum_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_2bar_lokl_small');
 
 melody_vae.initialize().then(() => changeButtonText("generate"));
+melody2bar_vae.initialize();
+drum_vae.initialize();
+
 music_vae = melody_vae
 
 Tone.start()
@@ -47,6 +52,9 @@ function processSample(sample) {
 }
 
 function genVAE() {
+  if (!music_vae.initialized) {
+    return;
+  }
   if (player.isPlaying()) {
     player.stop();
     changeButtonText("generate")
@@ -60,18 +68,16 @@ function genVAE() {
 }
 
 button.onclick = genVAE
-select.onchange = function () {
+select.onchange = function() {
     if (this.value == 'percussion') {
-        if (!drum_vae.initialized) {
-            changeButtonText("loading")
-            drum_vae.initialize().then(() => changeButtonText("generate"));
-        }
         music_vae = drum_vae
+    } else if (this.value == 'melody2bar') {
+        music_vae = melody2bar_vae
     } else if (this.value == 'melody') {
-        if (!melody_vae.initialized) {
-            changeButtonText("loading")
-            melody_vae.initialize().then(() => changeButtonText("generate"));
-        }
         music_vae = melody_vae
     }
 };
+slider.onchange = function() {
+    vae_temperature = this.value / 50.0
+    console.log(vae_temperature)
+}
